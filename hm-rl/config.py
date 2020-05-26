fd2254a0ac438583f56ad31525be8f71c6728c07
air <airmanhigh@gmail.com>
2020-05-26 17:36:09 +0800

    demo

--- hm-rl/config.py
+++ hm-rl/config.py
@@ -1,20 +1,29 @@
 import math
 
-UNIT_LEN_KM = 10  # 单位公路的长度，单位是km
 TIME_INTERVAL_DAY = 1  # 时间片
 DAY_PER_YEAR = 365  # 一年的天数
 MAX_TIME_DAY = DAY_PER_YEAR * 100  # 环境最多运行的天数
 REWARD_GAMMA = 0.99  # Exponentially Weighted Moving Average（指数加权滑动平均）方式计算 reward（奖赏）之和时所用的参数 gamma
 
 DEFAULT_HP = 100  # 每条公路的起始健康度
-DEFAULT_TRAFFIC = 100  # 默认起始每条公路的每日车流量
-DEFAULT_STATUS = "keep"  # 每条公路的默认状态
-DEFAULT_REMAIN = math.inf  # 默认状态的持续时间
+DEFAULT_TRAFFIC = 25000  # 默认起始每条公路的每日车流量
+DEFAULT_STATUS = {
+    "name": "in_use",
+    "remain": math.inf,
+    "stoppable": True
+}  # 每条公路的默认状态
 
 # 用字典代表每一条公路
 HIGHWAYS = [
-    {"name": "S1", "total_len": 20, "hp": 100, "traffic": 200},
-    {"name": "S2", "total_len": 40}
+    {"name": "GF", "traffic": int(26575444 / DAY_PER_YEAR / 2), "toll": int(190837600 / 26575444), "note": "广佛高速公路"},
+    {"name": "FK", "traffic": int(23968177 / DAY_PER_YEAR / 2), "toll": int(569280800 / 23968177), "note": "佛开高速公路"},
+    {"name": "JG", "traffic": int(30170232 / DAY_PER_YEAR / 2), "toll": int(541588200 / 30170232), "note": "京珠高速公路广珠段"},
+    {"name": "GH", "traffic": int(21450514 / DAY_PER_YEAR / 2), "toll": int(800781100 / 21450514), "note": "广惠高速公路"},
+    {"name": "HY", "traffic": int(17044547 / DAY_PER_YEAR / 2), "toll": int(106159000 / 17044547), "note": "惠盐高速公路"},
+    {"name": "GZ", "traffic": int(14315677 / DAY_PER_YEAR / 2), "toll": int(289978900 / 14315677), "note": "广肇高速公路"},
+    {"name": "JZ", "traffic": int(20429959 / DAY_PER_YEAR / 2), "toll": int(196259200 / 20429959), "note": "江中高速公路"},
+    {"name": "KD", "traffic": int(1187605 / DAY_PER_YEAR / 2), "toll": int(115537300 / 1187605), "note": "康大高速"},
+    {"name": "GK", "traffic": int(1158072 / DAY_PER_YEAR / 2), "toll": int(73247800 / 1158072), "note": "赣康高速"},
 ]
 
 
@@ -30,16 +39,24 @@ cost:
     采取动作所用的花销
 time:
     该动作的持续时间
-traffic:
+new_traffic:
     一个函数，计算动作持续期间的每日车流量，每隔单位时间会调用一次
     输入：状态剩余时间，旧车流量值
     输出：新车流量值
 """
 ACTIONS = [
-    {"name": "small", "new_hp": lambda x: x + 10, "cost": 50000, "time": 1, "traffic": lambda tm, tfc: 0},
-    {"name": "big", "new_hp": lambda x: x + 30, "cost": 200000, "time": 15, "traffic": lambda tm, tfc: 0},
-    {"name": "remake", "new_hp": lambda x: 100, "cost": 500000, "time": 60, "traffic": lambda tm, tfc: 0},
-    {"name": "keep", "new_hp": lambda x: x, "cost": 0, "time": math.inf, "traffic": lambda tm, tfc: tfc}
+    {"name": "small", "cost": 50000, "time": 2,
+     "new_hp": lambda x: x + 10, "new_traffic": lambda tm, tfc: 0,
+     "stoppable": False},
+    {"name": "big", "cost": 200000, "time": 20,
+     "new_hp": lambda x: x + 10, "new_traffic": lambda tm, tfc: 0,
+     "stoppable": False},
+    {"name": "remake", "cost": 500000, "time": 60,
+     "new_hp": lambda x: 100, "new_traffic": lambda tm, tfc: 0,
+     "stoppable": False},
+    {"name": "in_use", "cost": 0, "time": math.inf,
+     "new_hp": lambda x: x, "new_traffic": lambda tm, tfc: tfc,
+     "stoppable": True}
 ]
 
 """ 突发事件
@@ -50,12 +67,22 @@ frequency：
     每日发生概率
 """
 EMERGENCIES = [
-    {"name": "destroy", "new_hp": lambda x: 0, "frequency": 1 / DAY_PER_YEAR / 100, "type": "Poisson"},
-    {"name": "damage", "new_hp": lambda x: x / 2, "frequency": 1 / DAY_PER_YEAR / 10, "type": "Poisson"}
+    {"name": "destroy", "cost": 0, "time": 10,
+     "new_hp": lambda x: x - 20, "frequency": 1 / DAY_PER_YEAR / 100,
+     "stoppable": False},
+    {"name": "damage", "cost": 0, "time": 3,
+     "new_hp": lambda x: x - 5, "frequency": 1 / DAY_PER_YEAR / 10,
+     "stoppable": False},
+    {"name": "damage", "cost": 0, "time": 1,
+     "new_hp": lambda x: x - 1, "frequency": 1 / DAY_PER_YEAR / 1,
+     "stoppable": False}
 ]
 
 
-def reward(traffic, cost, hp):
+def reward(traffic: int,
+           toll: float,
+           hp: float,
+           cost: float):
     """ 单步奖赏计算函数
 
     Parameters
@@ -71,8 +98,9 @@ def reward(traffic, cost, hp):
     -------
 
     """
-    income = traffic * 300
-    return income + cost - 100 * hp
+    # 平均hp大约每日-0.1
+    income = traffic * toll
+    return (income - cost) / 10000 + 10 * hp - 1000
 
 
 def traffic_to_aging_speed(traffic: float) -> float:
@@ -88,9 +116,23 @@ def traffic_to_aging_speed(traffic: float) -> float:
     aging_speed : float
         new_hp = old_hp - aging_speed
     """
-    C = 1.49  # 换算系数
-    N = C * traffic  # 轴次
+
+    AADTT, DDF, LDF = traffic, 0.5, 0.4
+    EALF_parameters = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ]
+    EALF_m = [a * b + c * d for a, b, c, d in EALF_parameters]
+    N = AADTT * DDF * LDF + sum(EALF_m)  # 轴次
+
+    N = 1.49 * traffic
+
     N *= DAY_PER_YEAR
+
     aging = 1.164 + 6.621 * 10 ** (-7) * N + 3.381 * 10 ** (-14) * N * N  # 断面 PQI 降低值
     aging /= DAY_PER_YEAR
+
     return aging
--- hm-rl/hm_env.py
+++ hm-rl/hm_env.py
@@ -1,8 +1,7 @@
-from __future__ import annotations
-
 import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
+from gym.envs.registration import register
 
 import json
 from pprint import pprint
@@ -15,67 +14,85 @@ from typing import List, Iterator, Callable
 import config as cfg
 
 
+class Action:
+    def __init__(self,
+                 name,
+                 new_hp: Callable,
+                 new_traffic: Callable,
+                 cost,
+                 time,
+                 stoppable: bool):
+        self.name = name
+        self.new_hp: Callable = new_hp
+        self.cost = cost
+        self.remain_time = time
+        self.new_traffic: Callable = new_traffic
+        self.stoppable: bool = stoppable
+
+
+class Status:
+    def __init__(self,
+                 name: str,
+                 remain: float,
+                 stoppable: bool):
+        self.name: str = name
+        self.remain: float = remain
+        self.stoppable: bool = stoppable
+
+    def __str__(self):
+        return str({
+            "name": self.name,
+            "remain": self.remain,
+            "stoppable": self.stoppable
+        })
+
+    def __eq__(self, other: 'Status'):
+        return self.name == other.name and \
+               self.remain == other.remain and \
+               self.stoppable == other.stoppable
+
+
+class Emergency:
+    def __init__(self,
+                 name: str,
+                 new_hp: Callable,
+                 frequency: float):
+        self.name: str = name
+        self.new_hp: Callable = new_hp
+        self.frequency: float = frequency
+
+
 class Highway:
     def __init__(self,
                  name: str = 'None',
-                 length: float = 0,
                  hp: float = 0,
-                 traffic: float = 0,
-                 status: str = 'None',
-                 remain: float = math.inf,
-                 id: int = None,
+                 traffic: int = 0,
+                 toll: float = 0,
+                 status: Status = None,
+                 tag: int = None,
                  new_traffic: Callable = None):
         self.name: str = name
-        self.id = id
-        self.len: float = length  # in km
+        self.tag = tag
 
         self._hp: float = hp
-        self.traffic: float = traffic
-        self.new_traffic = new_traffic
+        # only be assigned when take actions, should be reset to None after action ends
+        self.new_hp_to_set: float = None
+        self.traffic: int = traffic
+        self.new_traffic: Callable = new_traffic
+        self.toll = toll
 
-        self.status: str = status
-        self.remain: float = remain
+        self.status: Status = status
+        self.default_status: Status = status
 
-        self.default_status: str = status
-        self.default_remain: float = remain
-
-    @staticmethod
-    def get_aging(traffic: float):
-        return cfg.traffic_to_aging_speed(traffic)
-
-    @staticmethod
-    def init_from_cfg(highway: dict) -> Iterator[Highway]:
-        hp: float = highway['hp'] if 'hp' in highway else cfg.DEFAULT_HP
-        traffic: float = highway['traffic'] if 'traffic' in highway else cfg.DEFAULT_TRAFFIC
-        new_traffic: Callable = highway['new_traffic'] if 'new_traffic' in highway else cfg.DEFAULT_NEW_TRAFFIC
-
-        for i in range(highway['total_len'] // cfg.UNIT_LEN_KM):
-            yield Highway(name=highway['name'],
-                          length=cfg.UNIT_LEN_KM,
-                          hp=hp,
-                          traffic=traffic,
-                          status=cfg.DEFAULT_STATUS,
-                          remain=cfg.DEFAULT_REMAIN,
-                          id=i,
-                          new_traffic=new_traffic)
-
-    def set_status(self,
-                   status: str,
-                   remain: float = math.inf):
+    def take_action(self,
+                    status: Status,
+                    new_traffic: Callable,
+                    new_hp: Callable):
         self.status = status
-        self.remain = remain
+        self.new_traffic = new_traffic
+        self.new_hp_to_set = new_hp(self._hp)
 
-    @property
-    def hp(self):
-        return self._hp
-
-    def set_hp(self, new_hp: float):
-        if new_hp >= 0:
-            self._hp = new_hp
-        else:
-            self._hp = 0
-
-    def update(self, period:int):
+    def update(self, period: int):
         assert isinstance(period, int)
         while period > 0:
             self.update_one_day()
@@ -83,32 +100,59 @@ class Highway:
 
     def update_one_day(self):
         # line
-        self.traffic = self.new_traffic(self.remain, self.traffic)
-        aging = cfg.traffic_to_aging_speed(self.traffic)
+        self.traffic = self.new_traffic(self.status.remain, self.traffic)
+        # TODO ugly design here
+        # if is being repaired, freeze hp
+        if self.status.name == self.default_status.name:
+            aging = cfg.traffic_to_aging_speed(self.traffic)
+            self.set_hp(self._hp - aging)
 
-        self._hp -= aging
-        if self._hp < 0:
-            self._hp = 0
-
-        self.remain -= 1
-        if self.remain <= 0:
+        self.status.remain -= 1
+        if self.status.remain <= 0:
+            # this means the action finally ends
+            # we should set new hp
             self.status = self.default_status
-            self.remain = self.default_remain
+            self.set_hp(self.new_hp_to_set)
+            self.new_hp_to_set = None
 
+    @staticmethod
+    def get_aging(traffic: float):
+        return cfg.traffic_to_aging_speed(traffic)
 
-class Action:
-    def __init__(self,
-                 name,
-                 new_hp: Callable,
-                 cost,
-                 time):
-        self.name = name
-        self.update_function = new_hp
-        self.cost = cost
-        self.remain_time = time
+    @staticmethod
+    def init_from_cfg(highway: dict) -> 'Highway':
+        hp: float = highway['hp'] if 'hp' in highway else cfg.DEFAULT_HP
+        traffic: int = highway['traffic'] if 'traffic' in highway else cfg.DEFAULT_TRAFFIC
+        new_traffic: Callable = highway['new_traffic'] if 'new_traffic' in highway else cfg.DEFAULT_NEW_TRAFFIC
+        return Highway(name=highway['name'],
+                       hp=hp,
+                       traffic=traffic,
+                       toll=highway['toll'],
+                       status=Status(**cfg.DEFAULT_STATUS),
+                       tag=0,
+                       new_traffic=new_traffic)
 
-    def get_new_hp(self, old_hp):
-        return self.update_function(old_hp)
+    def __str__(self):
+        d = {
+            "name": self.name,
+            "tag": self.tag,
+            "hp": self._hp,
+            "traffic": self.traffic,
+            "status": str(self.status)
+        }
+        return str(d)
+
+    @property
+    def hp(self):
+        return self._hp
+
+    def set_hp(self, new_hp: float):
+        if new_hp < 0:
+            self._hp = 0
+        elif new_hp > cfg.DEFAULT_HP:
+            self._hp = cfg.DEFAULT_HP
+        else:
+            self._hp = new_hp
 
 
 class HMEnv(gym.Env):
@@ -116,25 +160,24 @@ class HMEnv(gym.Env):
 
     def __init__(self):
         # init highways
-        self.highways: List[Highway] = []
-        for highway_dict in cfg.HIGHWAYS:
-            for highway in Highway.init_from_cfg(highway_dict):
-                self.highways.append(highway)
+        self.highways: List[Highway] = [Highway.init_from_cfg(d) for d in cfg.HIGHWAYS]
         # set TIME
         self.cur_time = 0
         self.time_interval = cfg.TIME_INTERVAL_DAY
         # set ACTIONS
         self.actions = [Action(**d_act) for d_act in cfg.ACTIONS]
         # (action tag, highway tag)
-        self.action_space = gym.spaces.MultiDiscrete([len(self.actions), len(self.highways)])
+        self.action_space = gym.spaces.MultiDiscrete([len(self.actions)] * len(self.highways))
         # set OBSERVATIONS
-        self.observation_space = gym.spaces.Box(low=0,
-                                                high=cfg.DEFAULT_HP,
-                                                shape=(len(self.highways),))
+        """ observation_space
+        [[       hp_1,        hp_2, ...,        hp_n],
+         [stoppable_1, stoppable_2, ..., stoppable_n]]
+        """
+        self.observation_space = gym.spaces.Box(0, cfg.DEFAULT_HP, (2, len(self.highways)))
         # set REWARDS
         self.reward_range = (-float('inf'), float('inf'))
         self.reward_gamma = cfg.REWARD_GAMMA
-        self.reward_weight_sum = 0
+        self.reward_ewma = 0
         # LOOP CONTROL
         self.should_stop = False
         self.max_time = cfg.MAX_TIME_DAY
@@ -145,42 +188,50 @@ class HMEnv(gym.Env):
         Parameters
         ----------
         action : array
-            (action_tag, highway_tag), both are in type int
 
         Returns
         -------
         result : tuple
-            (obs, rewards, dones, info)
+            (obs, rewards, done, info)
         """
 
-        # 1-th: take action
-        act_idx, hw_idx = action[0], action[1]  # action name and target
-        # update status
-        chosen_highway, chosen_action = self.highways[hw_idx], self.actions[act_idx]
-        chosen_highway.set_status(chosen_action.name, chosen_action.remain_time)
-        chosen_highway.set_hp(chosen_action.get_new_hp(chosen_highway.hp))
+        # 1-th: Take Action, Calculate reward
+        rewards = []
+        for hw_idx, act_idx in enumerate(action):
+            chosen_highway, chosen_action = self.highways[hw_idx], self.actions[act_idx]
+            if chosen_highway.status.stoppable:
+                # update status
+                # TODO ugly design here
+                new_status = Status(chosen_action.name, chosen_action.remain_time, chosen_action.stoppable)
+                chosen_highway.take_action(new_status, chosen_action.new_traffic, chosen_action.new_hp)
+            # reward
+            rewards.append(
+                cfg.reward(chosen_highway.traffic, chosen_highway.toll, chosen_highway.hp, chosen_action.cost))
+
         # 2-th update
         for hw in self.highways:
             hw.update(self.time_interval)
-        # 3-th calculate the REWARD
-        # TODO
-        reward = cfg.reward(sum(hw.traffic for hw in self.highways),
-                            chosen_action.cost,
-                            sum(hw.hp for hw in self.highways))
-        # 4-th time goes by
+        # 3-th time goes by
         self.cur_time += self.time_interval
         if self.cur_time > self.max_time:
             self.should_stop = True
-        # 5-th RETURN
-        obs = [hw.hp for hw in self.highways]
-        info = 'action name: {}'.format(chosen_action.name)
-        return obs, reward, self.should_stop, info
+        # 4-th RETURN
+        obs = np.array([[hw.hp for hw in self.highways],
+                        [int(hw.status.stoppable) for hw in self.highways]])
+        info: dict = {}
+        return obs, np.average(rewards), self.should_stop, info
 
     def reset(self):
         self.__init__()
+        obs = np.array([[hw.hp for hw in self.highways],
+                        [int(hw.status.stoppable) for hw in self.highways]])
+        return obs
 
     def render(self, mode='human'):
         pass
 
     def close(self):
         pass
+
+
+register(id='PccNs-v0', entry_point='network_sim:SimulatedNetworkEnv')
